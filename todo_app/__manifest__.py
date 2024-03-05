e92925dabc1531cb1a6a0559a287e54e67a92239
alibayomy <alibayomy12@gmail.com>
2024-03-05 02:09:59 +0200

    Creating the manifest file and the init.py file to start my todo odoo application

---
+++ todo_app/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Todo App",
+    'summary':"""""",
+    'description': """""",
+    'author': "Ali Bayomy",
+    'category': 'Productivity',
+    'version': '17.0.0.1.0',
+    'depends':['base'
+               ],
+    'application': True,
+    'data': [
+    ],
+}
